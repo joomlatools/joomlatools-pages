22177a4765f3d40f814f5bc4479cdf4f94e2a002
Johan Janssens <johan@timble.net>
2018-03-29 16:00:16 +0200

    #3 - Add support for data url aliasing

--- code/site/components/com_pages/template/locator/data.php
+++ code/site/components/com_pages/template/locator/data.php
@@ -43,9 +43,18 @@ class ComPagesTemplateLocatorData extends KTemplateLocatorFile
     {
         $result = parent::find($info);
 
-        //If the result is a directory, return all the files in the directory
-        if(is_dir($result)) {
-            $result = glob($result.'/*.*');
+        if($result)
+        {
+            //If the result is a file check if the first line is a url
+            if(!is_dir($result))
+            {
+                $line = trim(fgets(fopen($result, 'r')));
+                if(parse_url($line, PHP_URL_SCHEME)) {
+                    $result = $line;
+                }
+            }
+            //If the result is a directory, return all the files in the directory
+            else $result = glob($result.'/*.*');
         }
 
         return $result;
