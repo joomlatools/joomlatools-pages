d7eb61af4c008f24095c46a44704a738a1da7fb1
Johan Janssens <johan@timble.net>
2018-04-26 13:20:05 +0200

    #13 - Exclude html, css, js, txt and svg from rendering and allow to include the contents of the file in the template.

--- code/site/components/com_pages/template/template.php
+++ code/site/components/com_pages/template/template.php
@@ -9,9 +9,20 @@
 
 class ComPagesTemplate extends KTemplate
 {
+    protected $_excluded_types;
+
+
+    public function __construct(KObjectConfig $config)
+    {
+        parent::__construct($config);
+
+        $this->_excluded_types = KObjectConfig::unbox($config->excluded_types);
+    }
+
     protected function _initialize(KObjectConfig $config)
     {
         $config->append(array(
+            'excluded_types' => array('html', 'txt', 'svg', 'css', 'js'),
             'functions'  => array(
                 'data' => function($path, $format = '') {
                     return  $this->getObject('com:pages.data.factory')->createObject($path, $format);
@@ -39,5 +50,51 @@ class ComPagesTemplate extends KTemplate
         else $result = $date->format($format);
 
         return $result;
+    }
+
+    public function loadFile($url)
+    {
+        $locator = $this->getObject('template.locator.factory')->createLocator($url);
+
+        if (!$file = $locator->locate($url)) {
+            throw new InvalidArgumentException(sprintf('The template "%s" cannot be located.', $url));
+        }
+
+        $type = pathinfo($file, PATHINFO_EXTENSION);
+
+        if(!in_array($type, $this->_excluded_types))
+        {
+            //Create the template engine
+            $config = array(
+                'template' => $this,
+                'functions' => $this->_functions
+            );
+
+            $this->_source = $this->getObject('template.engine.factory')
+                ->createEngine($file, $config)
+                ->loadFile($url);
+        }
+        else KTemplateAbstract::loadFile($url);
+
+        return $this;
+    }
+
+    public function loadString($source, $type = null, $url = null)
+    {
+        if($type && !in_array($type, $this->_excluded_types))
+        {
+            //Create the template engine
+            $config = array(
+                'template'  => $this,
+                'functions' => $this->_functions
+            );
+
+            $this->_source = $this->getObject('template.engine.factory')
+                ->createEngine($type, $config)
+                ->loadString($source,  $url);
+        }
+        else KTemplateAbstract::loadString($source);
+
+        return $this;
     }
 }
