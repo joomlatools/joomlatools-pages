b77a7bb759a32b4b1726758232a92e01dfedb8ba
Johan Janssens <johan@timble.net>
2018-04-26 15:15:44 +0200

    #8 - Moved to framework and extedended list of excludes with css, js and svg types

--- code/site/components/com_pages/template/page.php
+++ code/site/components/com_pages/template/page.php
@@ -9,16 +9,6 @@
 
 class ComPagesTemplatePage extends KTemplate
 {
-    protected $_excluded_types;
-
-
-    public function __construct(KObjectConfig $config)
-    {
-        parent::__construct($config);
-
-        $this->_excluded_types = KObjectConfig::unbox($config->excluded_types);
-    }
-
     protected function _initialize(KObjectConfig $config)
     {
         $config->append(array(
@@ -34,51 +24,5 @@ class ComPagesTemplatePage extends KTemplate
         ));
 
         parent::_initialize($config);
-    }
-
-    public function loadFile($url)
-    {
-        $locator = $this->getObject('template.locator.factory')->createLocator($url);
-
-        if (!$file = $locator->locate($url)) {
-            throw new InvalidArgumentException(sprintf('The template "%s" cannot be located.', $url));
-        }
-
-        $type = pathinfo($file, PATHINFO_EXTENSION);
-
-        if(!in_array($type, $this->_excluded_types))
-        {
-            //Create the template engine
-            $config = array(
-                'template' => $this,
-                'functions' => $this->_functions
-            );
-
-            $this->_source = $this->getObject('template.engine.factory')
-                ->createEngine($file, $config)
-                ->loadFile($url);
-        }
-        else KTemplateAbstract::loadFile($url);
-
-        return $this;
-    }
-
-    public function loadString($source, $type = null, $url = null)
-    {
-        if($type && !in_array($type, $this->_excluded_types))
-        {
-            //Create the template engine
-            $config = array(
-                'template'  => $this,
-                'functions' => $this->_functions
-            );
-
-            $this->_source = $this->getObject('template.engine.factory')
-                ->createEngine($type, $config)
-                ->loadString($source,  $url);
-        }
-        else KTemplateAbstract::loadString($source);
-
-        return $this;
     }
 }
