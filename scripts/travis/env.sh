fe1bd3a60433312725768ae77bee764c7591a3f4
Ercan Ozkaya <ercan@timble.net>
2018-05-07 15:58:00 +0300

    #7: Add Travis files

---
+++ scripts/travis/env.sh
@@ -0,0 +1,5 @@
+
+export CURRENT_REPO=$(echo $TRAVIS_REPO_SLUG | awk -F'/' '{print $2}')
+export CURRENT_BRANCH=$(echo $TRAVIS_BRANCH | sed 's/\//-/g')
+export PKG_SUFFIX=$(if [[ $CURRENT_BRANCH =~ ^[0-9] ]]; then echo $CURRENT_BRANCH; else echo _$CURRENT_BRANCH; fi;)
+export PKG_NAME=com_$CURRENT_REPO$PKG_SUFFIX.zip
