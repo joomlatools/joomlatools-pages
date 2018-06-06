b4459a3ff9f44d614d3a9be185f977215ac5ff86
Johan Janssens <johan@timble.net>
2018-06-07 01:31:27 +0200

    #13 - Do not redirect if no view information exists in the request

--- code/site/components/com_pages/dispatcher/http.php
+++ code/site/components/com_pages/dispatcher/http.php
@@ -14,12 +14,24 @@ class ComPagesDispatcherHttp extends ComKoowaDispatcherHttp
         $request = parent::getRequest();
 
         //Hanlde the root
-        if(!isset($request->query->file))
+        if(!isset($request->query->path))
         {
             $request->query->file = 'index';
             $request->query->path = '.';
         }
 
         return $request;
+    }
+
+    protected function _actionDispatch(KDispatcherContextInterface $context)
+    {
+        $method = strtolower($context->request->getMethod());
+
+        if (!in_array($method, $this->getHttpMethods())) {
+            throw new KDispatcherExceptionMethodNotAllowed('Method not allowed');
+        }
+
+        $this->execute($method, $context);
+        $this->send($context);
     }
 }
