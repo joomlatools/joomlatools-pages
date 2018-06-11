950b4a30f64ba0ea22c16b040968e36593bd0e9e
Johan Janssens <johan@timble.net>
2018-06-11 02:18:02 +0200

    Docblocks

--- code/site/components/com_pages/template/helper/paginator.php
+++ code/site/components/com_pages/template/helper/paginator.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * Joomlatools Framework - https://www.joomlatools.com/developer/framework/
+ * Joomlatools Pages
  *
- * @copyright   Copyright (C) 2007 Johan Janssens and Timble CVBA. (http://www.timble.net)
+ * @copyright   Copyright (C) 2018 Johan Janssens and Timble CVBA. (http://www.timble.net)
  * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
- * @link        https://github.com/joomlatools/joomlatools-framework for the canonical source repository
+ * @link        https://github.com/joomlatools/joomlatools-pages for the canonical source repository
  */
 
 class ComPagesTemplateHelperPaginator extends KTemplateHelperPaginator
