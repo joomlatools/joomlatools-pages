858562eec36a3bb63df3d5fe2516c85cfed7071b
Johan Janssens <johan@timble.net>
2018-11-20 00:45:47 +0100

    #36 - Add markdown filter

---
+++ code/site/components/com_pages/template/filter/markdown.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Joomlatools Pages
+ *
+ * @copyright   Copyright (C) 2018 Johan Janssens and Timble CVBA. (http://www.timble.net)
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        https://github.com/joomlatools/joomlatools-pages for the canonical source repository
+ */
+
+class ComPagesTemplateFilterMarkdown extends KTemplateFilterMarkdown
+{
+    public function filter(&$text)
+    {
+        $engine = $this->getObject('template.engine.factory')
+                ->createEngine('markdown', array('template' => $this->getTemplate()));
+
+        $text = $engine->loadString($text)->render();
+    }
+}
