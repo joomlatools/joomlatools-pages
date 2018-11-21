3695a81df6a2ff6141b26d7da2610c582f9858a8
Johan Janssens <johan@timble.net>
2018-11-21 20:30:11 +0100

    #36 - Do not extend from KTemplateFilterMarkdown

--- code/site/components/com_pages/template/filter/markdown.php
+++ code/site/components/com_pages/template/filter/markdown.php
@@ -7,7 +7,7 @@
  * @link        https://github.com/joomlatools/joomlatools-pages for the canonical source repository
  */
 
-class ComPagesTemplateFilterMarkdown extends KTemplateFilterMarkdown
+class ComPagesTemplateFilterMarkdown extends KTemplateFilterAbstract
 {
     public function filter(&$text)
     {
