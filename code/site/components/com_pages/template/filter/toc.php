e227ccde30b473abd2914575ef1618dcd609b8e4
Johan Janssens <johan@timble.net>
2018-12-30 00:01:36 +0100

    #38 - Code cleanup

--- code/site/components/com_pages/template/filter/toc.php
+++ code/site/components/com_pages/template/filter/toc.php
@@ -7,7 +7,6 @@
  * @link        https://github.com/joomlatools/joomlatools-pages for the canonical source repository
  */
 
-
 class ComPagesTemplateFilterToc extends KTemplateFilterAbstract
 {
     protected function _initialize(KObjectConfig $config)
