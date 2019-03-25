1ed96af346e879a1878f63b31f68cd5c547a28c7
Johan Janssens <johan@timble.net>
2019-03-25 16:23:09 +0100

    #71 - Add ComPagesModelInterface to define getQuery() and getData() methods

---
+++ code/site/components/com_pages/model/interface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Joomlatools Pages
+ *
+ * @copyright   Copyright (C) 2018 Johan Janssens and Timble CVBA. (http://www.timble.net)
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        https://github.com/joomlatools/joomlatools-pages for the canonical source repository
+ */
+
+interface ComPagesModelInterface extends KModelInterface
+{
+    public function getQuery($count = false);
+
+    public function getData($query = null);
+}
