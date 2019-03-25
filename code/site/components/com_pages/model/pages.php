40e5158f101b2367d57792ae5899534104226372
Johan Janssens <johan@timble.net>
2019-03-25 02:52:09 +0100

    #71 - Refactor pages model
    Remove accessible, categorizable, collectable, crawlable, dateable,
    filterable behavior and move state filtering logic in the model.

--- code/site/components/com_pages/model/pages.php
+++ code/site/components/com_pages/model/pages.php
@@ -15,8 +15,18 @@ class ComPagesModelPages extends ComPagesModelCollection
         $this->getState()
             ->insert('path', 'url', '.')
             ->insert('slug', 'cmd', '', true, array('path'))
-            ->insert('recurse', 'boolean', true, false, array(), true) //internal state
-            ->insert('level', 'int', 0, false, array(), true);         //internal state
+            //Internal states
+            ->insert('recurse', 'boolean', true, false, array(), true)
+            ->insert('level', 'int', 0, false, array(), true)
+            //Filter states
+            ->insert('visible', 'boolean')
+            ->insert('collection', 'boolean')
+            ->insert('sitemap', 'boolean')
+            ->insert('category', 'cmd')
+            ->insert('year', 'int')
+            ->insert('month', 'int')
+            ->insert('day', 'int')
+            ->insert('published', 'boolean');
     }
 
     protected function _initialize(KObjectConfig $config)
@@ -25,13 +35,6 @@ class ComPagesModelPages extends ComPagesModelCollection
             'identity_key' => 'path',
             'behaviors'    => [
                 'recursable',
-                'dateable',
-                'sortable',
-                'categorizable',
-                'accessible',
-                'crawlable',
-                'visible',
-                'collectable',
             ]
         ]);
 
@@ -51,9 +54,7 @@ class ComPagesModelPages extends ComPagesModelCollection
             {
                 if ($this->getState()->isUnique())
                 {
-                    $page = $registry->getPage($path.'/'.$this->getState()->slug);
-
-                    if ($page) {
+                    if($page = $registry->getPage($path.'/'.$this->getState()->slug)) {
                         $pages = $page->toArray();
                     }
                 }
@@ -64,5 +65,114 @@ class ComPagesModelPages extends ComPagesModelCollection
         }
 
         return $this->_data;
+    }
+
+    public function filterData($item, KModelStateInterface $state)
+    {
+        $result = true;
+
+        //Visible
+        if(!is_null($state->visible))
+        {
+            if($state->visible === true) {
+                $result = !isset($item['visible']) || $item['visible'] !== false;
+            }
+
+            if($state->visible === false) {
+                $result = isset($item['visible']) && $item['visible'] === false;
+            }
+        }
+
+        //Published
+        if($result &&  !is_null($state->published))
+        {
+            if($state->published === true) {
+                $result = !isset($item['published']) || $item['published'] !== false;
+            }
+
+            if($state->published === false) {
+                $result = isset($item['published']) && $item['published'] === false;
+            }
+        }
+
+        //Collection
+        if($result && !is_null($state->collection))
+        {
+            if($state->collection === true) {
+                $result = isset($item['collection']) && $item['collection'] !== false;
+            }
+
+            if($state->collection === false) {
+                $result = !isset($item['collection']) || $item['collection'] === false;
+            }
+        }
+
+        //Sitemap
+        if($result && (bool) $state->sitemap) {
+            $result = (isset($item['sitemap']) && $item['sitemap'] == false) ? false : true;
+        }
+
+        //Category
+        if($result && (bool) $state->category) {
+            $result =  isset($item['category']) && $item['category'] == $state->category;
+        }
+
+        //Date
+        if($result &&  (bool) ($state->year || $state->month || $state->day))
+        {
+            if(isset($item['date']))
+            {
+                //Get the timestamp
+                if(!is_integer($item['date'])) {
+                    $date = strtotime($item['date']);
+                } else {
+                    $date = $item['date'];
+                }
+
+                if($state->year) {
+                    $result = ($state->year == date('Y', $date));
+                }
+
+                if($result && $state->month) {
+                    $result = ($state->month == date('m', $date));
+                }
+
+                if($result && $state->day) {
+                    $result = ($state->day == date('d', $date));
+                }
+            }
+        }
+
+        //Permissions
+        if($result)
+        {
+            //Goups
+            if(isset($item['access']['groups']))
+            {
+                $groups = $this->getObject('com:pages.database.table.groups')
+                    ->select($this->getObject('user')->getGroups(), KDatabase::FETCH_ARRAY_LIST);
+
+                $groups = array_map('strtolower', array_column($groups, 'title'));
+
+                if(!array_intersect($groups, $item['access']['groups'])) {
+                    $result = false;
+                }
+            }
+
+            //Roles
+            if($result && isset($item['access']['roles']))
+            {
+                $roles = $this->getObject('com:pages.database.table.roles')
+                    ->select($this->getObject('user')->getRoles(), KDatabase::FETCH_ARRAY_LIST);
+
+                $roles = array_map('strtolower', array_column($roles, 'title'));
+
+                if(!array_intersect($roles, $item['access']['roles'])) {
+                    $result = false;
+                }
+            }
+        }
+
+        return $result;
     }
 }
