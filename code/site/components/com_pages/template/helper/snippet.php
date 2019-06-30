ff3716e7253b29c4fd4e414d302f1e687d914bdb
Johan Janssens <johan@janssens.me>
2019-06-30 08:51:10 +0200

    #146 - Implement snippet helper
    Helper defines two methods a define() method to create a snippet and an
    expand() method to render a snippet.
    
    A snipppet is expanded using heredoc syntax. Heredoc text behaves just
    like a double-quoted string, without the double quotes. This means that
    quotes in a heredoc do not need to be escaped.

---
+++ code/site/components/com_pages/template/helper/snippet.php
@@ -0,0 +1,37 @@
+<?php
+
+class ExtensionTemplateHelperSnippet extends ComPagesTemplateHelperAbstract
+{
+    private $__snippets = array();
+
+    public function define(string $name, string $snippet, $overwrite = false)
+    {
+        if(!isset($this->__snippets[$name]) || $overwrite) {
+            $this->__snippets[$name] = $snippet;
+        }
+    }
+
+    public function expand(string $name, array $variables = array())
+    {
+        $result = false;
+
+        if(isset($this->__snippets[$name]))
+        {
+            $snippet = $this->__snippets[$name];
+
+            //Use the stream buffer to evaluate the partial
+            $str = "<?php \n return <<<SNIPPET\n$snippet\nSNIPPET;\n";
+
+            $buffer = $this->getObject('filesystem.stream.factory')->createStream('koowa-buffer://temp', 'w+b');
+            $buffer->truncate(0);
+            $buffer->write($str);
+
+            extract($variables, EXTR_OVERWRITE);
+
+            $result = include $buffer->getPath();
+        }
+
+        return $result;
+    }
+}
+
