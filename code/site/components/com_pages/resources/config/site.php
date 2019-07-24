50e7405d7d9270383328f8031c2c2919f6ec2e55
Johan Janssens <johan@janssens.me>
2019-07-24 05:28:52 +0200

    #163 - Get site paths through config to improve consistency and flexibilty

--- code/site/components/com_pages/resources/config/site.php
+++ code/site/components/com_pages/resources/config/site.php
@@ -10,44 +10,35 @@
 return [
 
     'identifiers' => [
-        'com:pages.page.locator' => [
-            'base_path' => $base_path
-        ],
-        'com:pages.data.locator' => [
-            'base_path' => $base_path.'/data'
-        ],
-        'com:pages.template.locator.theme' => [
-            'base_path' => $base_path.'/theme',
-        ],
         'com://site/pages.template.filter.asset' => [
             'schemes' =>  $config['aliases'] ?? array()
         ],
         'page.registry' => [
             'cache'       => $config['page_cache'] ?? (JDEBUG ? false : true),
             'cache_time'  => $config['page_cache_time'] ?? 60*60*24, //1d
-            'cache_path'  => $config['page_cache_path'] ?? $base_path.'/cache',
+            'cache_path'  => $config['page_cache_path'] ?? null,
             'collections' => $config['collections'] ?? array(),
             'redirects'   => isset($config['redirects']) ? array_flip($config['redirects']) : array(),
         ],
         'data.registry' => [
             'cache'      => $config['data_cache'] ?? (JDEBUG ? false : true),
             'cache_time' => $config['data_cache_time'] ?? 60*60*24, //1d
-            'cache_path' => $config['data_cache_path'] ?? $base_path.'/cache',
+            'cache_path' => $config['data_cache_path'] ?? null,
         ],
         'template.engine.factory' => [
             'cache'      => $config['template_cache'] ?? (JDEBUG ? false : true),
-            'cache_path' => $config['template_cache_path'] ?? $base_path.'/cache',
+            'cache_path' => $config['template_cache_path'] ?? null,
         ],
         'com://site/pages.dispatcher.behavior.cacheable' => [
             'cache'             => $config['http_cache'] ?? false,
-            'cache_path'        => $config['http_cache_path'] ?? $base_path.'/cache',
+            'cache_path'        => $config['http_cache_path'] ?? null,
             'cache_time'        => $config['http_cache_time']       ?? 60*15,  //15min
             'cache_time_shared' => $config['http_cache_time_proxy'] ?? 60*60*2, //2h
         ],
         'com://site/pages.data.client' => [
             'cache'      => $config['remote_cache'] ?? (JDEBUG ? false : true),
             'cache_time' => $config['remote_cache_time'] ?? 60*60*24, //1d
-            'cache_path' => $config['remote_cache_path'] ??  $base_path.'/cache',
+            'cache_path' => $config['remote_cache_path'] ??  null,
         ],
         'com://site/pages.model.entity.page' => [
             'data' => [
