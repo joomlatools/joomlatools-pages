d15cce7709ad0967b5cfb5f7224ff3c18cf17a84
Johan Janssens <johan@janssens.me>
2019-11-09 19:38:15 +0100

    #188 - If no redirect is defined for the form send a RESET_CONTENT status

--- code/site/components/com_pages/controller/form.php
+++ code/site/components/com_pages/controller/form.php
@@ -12,7 +12,7 @@ class ComPagesControllerForm extends ComPagesControllerPage
     protected function _initialize(KObjectConfig $config)
     {
         $config->append(array(
-            'behaviors' => ['validatable', 'processable'],
+            'behaviors' => ['processable'],
         ));
 
         parent::_initialize($config);
@@ -23,31 +23,17 @@ class ComPagesControllerForm extends ComPagesControllerPage
         parent::__construct($config);
 
         //Setup callbacks
-        $this->addCommandCallback('before.submit', '_validateData');
         $this->addCommandCallback('after.submit' , '_processData');
-    }
-
-    protected function _validateData(KControllerContextInterface $context)
-    {
-        $page = $this->getPage();
-
-        //Validate the request
-        $this->setHoneypot($page->form->honeypot);
-        $this->setValidationRules((array) KObjectConfig::unbox($this->getPage()->form->fields));
-        $this->validateRequest();
     }
 
     protected function _processData(KControllerContextInterface $context)
     {
-        if($this->isValidRequest())
-        {
-            $page = $this->getPage();
+        $page = $this->getPage();
 
-            $channel    = $page->form->name ?? $page->slug;
-            $processors = (array) KObjectConfig::unbox($page->form->processors);
+        $channel    = $page->form->name ?? $page->slug;
+        $processors = (array) KObjectConfig::unbox($page->form->processors);
 
-            $this->processData($context->request->data->toArray(), $processors, $channel);
-        }
+        $this->processData($context->request->data->toArray(), $processors, $channel);
     }
 
     protected function _actionSubmit(KControllerContextInterface $context)
@@ -57,5 +43,6 @@ class ComPagesControllerForm extends ComPagesControllerPage
             $url = $this->getView()->getRoute($redirect);
             $this->getResponse()->setRedirect($url);
         }
+        else $context->response->setStatus(KHttpResponse::RESET_CONTENT);
     }
 }
