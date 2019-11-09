2ea01db5fdd61fe9801984f2e4d883eba4270f0d
Johan Janssens <johan@janssens.me>
2019-11-09 18:42:29 +0100

    #188 - Add isSubmittable() method to check if a page can be submitted

--- code/site/components/com_pages/page/object.php
+++ code/site/components/com_pages/page/object.php
@@ -23,24 +23,34 @@ class ComPagesPageObject extends ComPagesObjectConfigFrontmatter
 
         return $type;
     }
-    
-    public function isCollection()
-    {
-        return isset($this->collection) && $this->collection !== false ? KObjectConfig::unbox($this->collection) : false;
-    }
 
     public function isForm()
     {
         return isset($this->form) && $this->form !== false ? KObjectConfig::unbox($this->form) : false;
     }
 
+    public function isCollection()
+    {
+        return isset($this->collection) && $this->collection !== false ? KObjectConfig::unbox($this->collection) : false;
+    }
+
     public function isReadable()
     {
-        return ($this->layout || $this->getContent());
+        $result = true;
+        if($this->format == 'html' ) {
+            $result = $this->layout || $this->getContent();
+        }
+
+        return $result;
+    }
+
+    public function isSubmittable()
+    {
+        return $this->isForm() && isset($this->form->fields);
     }
 
     public function isEditable()
     {
-        return $this->isForm() || ($this->isCollection() && isset($this->collection->fields));
+        return $this->isCollection() && isset($this->collection->fields);
     }
 }
