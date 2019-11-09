2ea743c15db5b1c70ce822d40b708f508e14b250
Johan Janssens <johan@janssens.me>
2019-11-09 19:30:30 +0100

    #188 - Add model exceptions to handle persist failures
    - Add ComPagesModelExceptionConflict
    - Add ComPagesModelExceptionError

---
+++ code/site/components/com_pages/model/exception/exception.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Joomlatools Framework - https://www.joomlatools.com/developer/framework/
+ *
+ * @copyright   Copyright (C) 2007 Johan Janssens and Timble CVBA. (http://www.timble.net)
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        https://github.com/joomlatools/joomlatools-framework for the canonical source repository
+ */
+
+/**
+ * Model Exception Interface
+ *
+ * @author  Johan Janssens <https://github.com/johanjanssens>
+ * @package Koowa\Library\Controller\Exception
+ */
+interface ComPagesModelException extends KException {}
