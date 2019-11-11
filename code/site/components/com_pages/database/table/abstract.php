8e9653c3d06c29b9b7b42835e1530db723ebae84
Johan Janssens <johan@janssens.me>
2019-11-11 18:35:10 +0100

    #188 - Only set the identity key for the autoinc column

--- code/site/components/com_pages/database/table/abstract.php
+++ code/site/components/com_pages/database/table/abstract.php
@@ -9,5 +9,22 @@
 
 abstract class ComPagesDatabaseTableAbstract extends KDatabaseTableAbstract
 {
+    public function __construct(KObjectConfig $config)
+    {
+        parent::__construct($config);
 
+        //Reset identity column only re-adding it for an auto increment primary key
+        $this->_identity_column = null;
+        unset($this->_column_map['id']);
+
+        foreach ($this->getColumns(true) as $column)
+        {
+            if ($column->autoinc)
+            {
+                $this->_identity_column  = $column->name;
+                $this->_column_map['id'] = $column->name;
+                break;
+            }
+        }
+    }
 }
