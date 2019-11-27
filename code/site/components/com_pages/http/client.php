073f05912e8e80ed514dc54dce30b9e05c78e54c
Johan Janssens <johan@janssens.me>
2019-11-27 05:25:26 +0100

    #212 - Code cleanup

--- code/site/components/com_pages/http/client.php
+++ code/site/components/com_pages/http/client.php
@@ -12,7 +12,7 @@ class ComPagesHttpClient extends KHttpClient
     protected function _initialize(KObjectConfig $config)
     {
         $config->append([
-            'cache'       => JDEBUG ? false : true,
+            'cache'       => false,
             'cache_path'  => $this->getObject('com://site/pages.config')->getSitePath('cache'),
             'cache_time'  => 60*60*24, //1 day https://tools.ietf.org/html/rfc7234#section-4.2.2
             'cache_force' => false,
@@ -33,6 +33,10 @@ class ComPagesHttpClient extends KHttpClient
                 if (!$cache = require($file)) {
                     throw new RuntimeException(sprintf('The response "%s" cannot be loaded from cache.', $file));
                 }
+
+                $cache = $this->getObject('http.response')
+                    ->setHeaders($cache['headers'])
+                    ->setContent($cache['content']);
 
                 if($request->isGet()) {
                     $response = $this->_validateGet($request, $cache);
@@ -73,23 +77,23 @@ class ComPagesHttpClient extends KHttpClient
         return $response;
     }
 
-    public function _validateGet(KHttpRequestInterface $request, array $cache)
+    public function _validateGet(KHttpRequestInterface $request, KHttpResponseInterface $cache)
     {
-        $url  = $request->getUrl();
+        $url = $request->getUrl();
 
         //Cache is not fresh
-        if(!$this->isCached($url))
+        if(!$this->isCached($url) || ($cache->isStale() && !$this->getConfig()->cache_force))
         {
             //Cache Validation
             //See: https://tools.ietf.org/html/rfc7234#section-4.3
-            if($cache['content'] && (isset($cahe['headers']['Etag']) || isset($cache['headers']['Last-Modified'])))
+            if($cache->getContent() && $cache->isValidateable())
             {
-                if(isset($cache['headers']['Etag'])) {
-                    $request->getHeaders()->set('If-None-Match', $cache['headers']['Etag']);
+                if($cache->getHeaders()->has('ETag')) {
+                    $request->getHeaders()->set('If-None-Match', $cache->getHeaders()->get('ETag'));
                 }
 
-                if(isset($cache['headers']['Last-Modified'])) {
-                    $request->getHeaders()->set('If-Modified-Since', $cache['headers']['Last-Modified']);
+                if($cache->getHeaders()->has('Last-Modified')) {
+                    $request->getHeaders()->set('If-Modified-Since', $cache->getHeaders()->get('Last-Modified'));
                 }
 
                 //Send validation request
@@ -100,8 +104,8 @@ class ComPagesHttpClient extends KHttpClient
                 {
                     //Freshening stored responses upon validation
                     //See: https://tools.ietf.org/html/rfc7234#section-4.3.4
-                    $response->setHeaders(array_merge($cache['headers'], $response->getHeaders()->toArray()));
-                    $response->setContent($cache['content']);
+                    $response->setHeaders(array_merge($cache->getHeaders()->toArray(), $response->getHeaders()->toArray()));
+                    $response->setContent($cache->getContent());
                 }
             }
             //Unconditional GET request
@@ -117,54 +121,54 @@ class ComPagesHttpClient extends KHttpClient
         else
         {
             $response = $this->getObject('http.response')
-                ->setHeaders($cache['headers'])
-                ->setContent($cache['content']);
+                ->setHeaders($cache->getHeaders()->toArray())
+                ->setContent($cache->getContent());
         }
 
         return $response;
     }
 
-    public function _validateHead(KHttpRequestInterface $request, array $cache)
+    public function _validateHead(KHttpRequestInterface $request, KHttpResponseInterface $cache)
     {
-        $url  = $request->getUrl();
+        $url = $request->getUrl();
 
         //Cache is not fresh
-        if(!$this->isCached($url))
+        if(!$this->isCached($url) || ($cache->isStale() && !$this->getConfig()->cache_force))
         {
             $response = parent::send($request);
 
             //Freshening stored responses via HEAD
             //See: https://tools.ietf.org/html/rfc7234#section-4.3.5
-            if(isset($cache['headers']['Etag']) || isset($cache['headers']['Last-Modified']))
+            if($cache->isValidateable())
             {
-                if (isset($cache['headers']['Etag']))
+                if($cache->getHeaders()->has('ETag'))
                 {
-                    if ($cache['headers']['Etag'] != $response->getHeaders()->get('Etag')) {
+                    if ($cache->getHeaders()->get('ETag') != $response->getHeaders()->get('ETag')) {
                         $cache['content'] = '';
                     }
                 }
 
-                if (isset($cache['headers']['Last-Modified']))
+                if($cache->getHeaders()->has('Last-Modified'))
                 {
-                    if ($cache['headers']['Last-Modified'] != $response->getHeaders()->get('Last-Modified')) {
+                    if ($cache->getHeaders()->get('Last-Modified') != $response->getHeaders()->get('Last-Modified')) {
                         $cache['content'] = '';
                     }
                 }
             }
 
-            $response->setHeaders(array_merge($cache['headers'], $response->getHeaders()->toArray()));
+            $response->setHeaders(array_merge($cache->getHeaders()->toArray(), $response->getHeaders()->toArray()));
 
             //Update the cache
             $this->storeCache($url, [
                 'headers' => $response->getHeaders()->toArray(),
-                'content' => $cache['content']
+                'content' => $cache->getContent()
             ]);
         }
         //Cache is fresh
         else
         {
             $response = $this->getObject('http.response')
-                ->setHeaders($cache['headers']);
+                ->setHeaders($cache->getHeaders()->toArray());
         }
 
         return $response;
@@ -174,14 +178,14 @@ class ComPagesHttpClient extends KHttpClient
     {
         if($this->getConfig()->cache)
         {
-            $path = $this->getConfig()->cache_path;
+            $path = rtrim($this->getConfig()->cache_path, '/');
 
             if(!is_dir($path) && (false === @mkdir($path, 0755, true) && !is_dir($path))) {
-                throw new RuntimeException(sprintf('The url cache path "%s" does not exist', $path));
+                throw new RuntimeException(sprintf('The resource cache path "%s" does not exist', $path));
             }
 
             if(!is_writable($path)) {
-                throw new RuntimeException(sprintf('The url cache path "%s" is not writable', $path));
+                throw new RuntimeException(sprintf('The resource cache path "%s" is not writable', $path));
             }
 
             if(!is_string($data))
@@ -216,7 +220,7 @@ class ComPagesHttpClient extends KHttpClient
         if($this->getConfig()->cache)
         {
             $hash = crc32($url.PHP_VERSION);
-            $path = $this->getConfig()->cache_path;
+            $path = rtrim($this->getConfig()->cache_path, '/');
 
             $cache  = $path.'/resource_'.$hash.'.php';
             $result = is_file($cache) ? $cache : false;
