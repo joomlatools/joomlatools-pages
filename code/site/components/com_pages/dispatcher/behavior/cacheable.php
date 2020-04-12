820f5ab587d296ebac30a0d3805e1ee461ca0cfd
Johan Janssens <johan@janssens.me>
2020-04-13 01:52:58 +0200

    #308 - Code cleanup

--- code/site/components/com_pages/dispatcher/behavior/cacheable.php
+++ code/site/components/com_pages/dispatcher/behavior/cacheable.php
@@ -210,10 +210,10 @@ class ComPagesDispatcherBehaviorCacheable extends KDispatcherBehaviorCacheable
             //In case cache exists delete it
             $this->deleteCache($this->getCacheKey());
 
-            //error_log( (string)(string)$response->getRequest()->getUrl()."\n", 3, '/var/www/joomlatools/debug.txt');
-
             //Set Cache-Control to no-store if response is not cacheable
             $context->getResponse()->getHeaders()->set('Cache-Control', ['no-store']);
+
+            //error_log( (string)(string)$response->getRequest()->getUrl()."\n", 3, '/var/www/joomlatools/debug.txt');
         }
     }
 
