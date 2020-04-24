8563c38ae31dc0b7336ce18cfe442b3888e951c7
yiendos <yiendos@sbmatrix.com>
2020-04-24 13:47:08 +0100

    #321: hopefully finishing the install process

--- .gitpod/bootstrap.sh
+++ .gitpod/bootstrap.sh
@@ -14,5 +14,15 @@ composer global require joomlatools/console --no-interaction
 /home/gitpod/.composer/vendor/bin/joomla site:install preview --www="${GITPOD_REPO_ROOT}" --mysql-login=root:
 
 # Symlink Joomlatools FW and Pages into the site
+composer require joomlatools/framework:3.* --working-dir="${GITPOD_REPO_ROOT}/preview" --ignore-platform-reqs
+
+/home/gitpod/.composer/vendor/bin/joomla extension:symlink preview joomlatools-pages --www="${GITPOD_REPO_ROOT}"  --projects-dir="/workspace"
+/home/gitpod/.composer/vendor/bin/joomla extension:install preview joomlatools-pages --www="${GITPOD_REPO_ROOT}"
 
 # Create default first page, see https://github.com/joomlatools/joomlatools-pages/wiki/Quick-Start
+
+mkdir -p /workspace/joomlatools-pages/preview/joomlatools-pages/pages/
+
+touch /workspace/joomlatools-pages/preview/joomlatools-pages/pages/hello.html.php
+
+echo "<h1>Hello sexy, where have you been?</h1>" > /workspace/joomlatools-pages/preview/joomlatools-pages/pages/hello.html.php
