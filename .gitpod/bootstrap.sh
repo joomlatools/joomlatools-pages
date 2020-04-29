2370ddad298520e5855b9a18a90fa7b88d93f4e2
yiendos <yiendos@sbmatrix.com>
2020-04-29 12:19:10 +0100

    321: update apache.conf and update install locations

--- .gitpod/bootstrap.sh
+++ .gitpod/bootstrap.sh
@@ -6,7 +6,7 @@
 ## That and it  takes a ridiculous amount of time to wait for the site
 
 # Create the document root
-mkdir "${GITPOD_REPO_ROOT}/preview"
+mkdir "/var/www/preview"
 
 # Install Composer dependencies
 composer install  --no-interaction
@@ -18,25 +18,24 @@ composer global require joomlatools/console --no-interaction
 mysql -e "SET GLOBAL sql_mode = 'NO_ENGINE_SUBSTITUTION'; SET SESSION sql_mode = 'NO_ENGINE_SUBSTITUTION';"
 
 # Set up a new Joomla site
-/home/gitpod/.composer/vendor/bin/joomla site:download preview --www="${GITPOD_REPO_ROOT}"
-/home/gitpod/.composer/vendor/bin/joomla site:install preview --www="${GITPOD_REPO_ROOT}" --mysql-login=root:
+/home/gitpod/.composer/vendor/bin/joomla site:download preview
+/home/gitpod/.composer/vendor/bin/joomla site:install preview --mysql-login=root:
 
 # Symlink Joomlatools FW and Pages into the site
-composer require joomlatools/framework:3.* --working-dir="${GITPOD_REPO_ROOT}/preview" --ignore-platform-reqs
+composer require joomlatools/framework:3.* --working-dir="/var/www/preview" --ignore-platform-reqs
 
-/home/gitpod/.composer/vendor/bin/joomla extension:symlink preview joomlatools-pages --www="${GITPOD_REPO_ROOT}"  --projects-dir="/workspace"
+/home/gitpod/.composer/vendor/bin/joomla extension:symlink preview joomlatools-pages  --projects-dir="/workspace"
 
 #extension install fubar... most probably command related I remember:
 #joomla site:create testing --symlink=joomlatools-pages,joomlatools-framework
 #works out of the box where as extension:install doesn't
-/home/gitpod/.composer/vendor/bin/joomla extension:install preview joomlatools-pages --www="${GITPOD_REPO_ROOT}"
+/home/gitpod/.composer/vendor/bin/joomla extension:install preview joomlatools-pages
 
 #ensure that the componnent can be found, enable and correct state
 mysql -uroot  sites_preview < /workspace/joomlatools-pages/.gitpod/sites_preview.sql
 
+mkdir -p /var/www/preview/joomlatools-pages/pages/
 
-mkdir -p /workspace/joomlatools-pages/preview/joomlatools-pages/pages/
-
-cp /workspace/joomlatools-pages/.gitpod/hello.html.php /workspace/joomlatools-pages/preview/joomlatools-pages/pages/hello.html.php
+cp /workspace/joomlatools-pages/.gitpod/hello.html.php /var/www/preview/joomlatools-pages/pages/hello.html.php
 
 
