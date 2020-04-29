0be96429544af4abc016f0efaf375cf0028b9452
Steven Rombauts <steven@kotuha.be>
2020-04-29 15:03:19 +0200

    #321: Echo description for each description in bootstrap.sh

--- .gitpod/bootstrap.sh
+++ .gitpod/bootstrap.sh
@@ -5,31 +5,31 @@
 ## it is only installed  after the gitpod has been initialised
 ## That and it  takes a ridiculous amount of time to wait for the site
 
-# Create the document root
-mkdir "/var/www/preview"
+echo "Create the document root"
+mkdir -p "/var/www/preview"
 
-# Install Composer dependencies
+echo "Install Composer dependencies"
 composer install  --no-interaction
 
-# Install joomla/console
+echo "Install joomla/console"
 composer global require joomlatools/console --no-interaction
 
-# Add joomla/console to the PATH in the current session
+echo "Add joomla/console to the PATH in the current session"
 export PATH=/home/gitpod/.composer/vendor/bin/:$PATH
 
-# Make sure PATH is always updated
+echo "Make sure PATH is always updated"
 echo "export PATH=/home/gitpod/.composer/vendor/bin/:$PATH" >> ~/.bashrc
 
-# Install the joomlatools/console-joomlatools helper plugin
+echo "Install the joomlatools/console-joomlatools helper plugin"
 joomla plugin:install joomlatools/console-joomlatools:dev-master
 
-# Clone Joomlatools FW
-git clone -b master -–depth 1 https://github.com/joomlatools/joomlatools-framework.git /workspace/joomlatools-framework
+echo "Clone Joomlatools FW"
+git clone -b master --depth 1 git@github.com:joomlatools/joomlatools-framework.git /workspace/joomlatools-framework
 
 #ERROR 1292 (22007): Incorrect datetime value: '0000-00-00 00:00:00' for column 'checked_out_time' at row 1
 mysql -e "SET GLOBAL sql_mode = 'NO_ENGINE_SUBSTITUTION'; SET SESSION sql_mode = 'NO_ENGINE_SUBSTITUTION';"
 
-# Set up a new Joomla site
+echo "Set up a new Joomla site"
 joomla site:download preview
 joomla site:install preview --mysql-login=root: --symlink=joomlatools-pages --projects-dir="/workspace"
 
