0b933a6ec10ccc38945337675a1de5e32599dc72
Steven Rombauts <steven@kotuha.be>
2020-04-29 14:50:15 +0200

    #321: Clone joomlatools/framework, install joomlatools/console-joomlatools plugin and update symlinks

--- .gitpod/bootstrap.sh
+++ .gitpod/bootstrap.sh
@@ -14,22 +14,24 @@ composer install  --no-interaction
 # Install joomla/console
 composer global require joomlatools/console --no-interaction
 
+# Add joomla/console to the PATH in the current session
+export PATH=/home/gitpod/.composer/vendor/bin/:$PATH
+
+# Make sure PATH is always updated
+echo "export PATH=/home/gitpod/.composer/vendor/bin/:$PATH" >> ~/.bashrc
+
+# Install the joomlatools/console-joomlatools helper plugin
+joomla plugin:install joomlatools/console-joomlatools:dev-master
+
+# Clone Joomlatools FW
+git clone -b master -–depth 1 https://github.com/joomlatools/joomlatools-framework.git /workspace/joomlatools-framework
+
 #ERROR 1292 (22007): Incorrect datetime value: '0000-00-00 00:00:00' for column 'checked_out_time' at row 1
 mysql -e "SET GLOBAL sql_mode = 'NO_ENGINE_SUBSTITUTION'; SET SESSION sql_mode = 'NO_ENGINE_SUBSTITUTION';"
 
 # Set up a new Joomla site
-/home/gitpod/.composer/vendor/bin/joomla site:download preview
-/home/gitpod/.composer/vendor/bin/joomla site:install preview --mysql-login=root:
-
-# Symlink Joomlatools FW and Pages into the site
-composer require joomlatools/framework:3.* --working-dir="/var/www/preview" --ignore-platform-reqs
-
-/home/gitpod/.composer/vendor/bin/joomla extension:symlink preview joomlatools-pages  --projects-dir="/workspace"
-
-#extension install fubar... most probably command related I remember:
-#joomla site:create testing --symlink=joomlatools-pages,joomlatools-framework
-#works out of the box where as extension:install doesn't
-/home/gitpod/.composer/vendor/bin/joomla extension:install preview joomlatools-pages
+joomla site:download preview
+joomla site:install preview --mysql-login=root: --symlink=joomlatools-pages --projects-dir="/workspace"
 
 #ensure that the componnent can be found, enable and correct state
 mysql -uroot  sites_preview < /workspace/joomlatools-pages/.gitpod/sites_preview.sql
