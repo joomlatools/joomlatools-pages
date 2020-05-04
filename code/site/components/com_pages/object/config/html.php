cdf94454293f29d82c64f2c8fea5c9a98cc6c196
Johan Janssens <johan@janssens.me>
2020-05-04 04:31:24 +0200

    #319 - Refactor the xml and html config objects
    - Return an interatable ComPagesDomDocument object instead of an array
    from fromString()
    
    - Add a fromArray() method to accept another config object or an array
    and return a ComPagesDomDocument object

--- code/site/components/com_pages/object/config/html.php
+++ code/site/components/com_pages/object/config/html.php
@@ -13,70 +13,22 @@ class ComPagesObjectConfigHtml extends ComPagesObjectConfigXml
 
     public function fromString($string, $object = true)
     {
-        $data = array();
-
-        if(!empty($string))
-        {
-            $dom = new DOMDocumentHtml('1.0', 'UTF-8');
-            libxml_use_internal_errors(true);
-
-            $dom->normalizeDocument();
-            $dom->preserveWhiteSpace = false;
-
-            if($dom->loadHtml($string, LIBXML_COMPACT, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD | LIBXML_NOBLANKS) === false) {
-                throw new DomainException('Cannot parse HTML string');
-            }
-
-            libxml_use_internal_errors(false);
-
-            $data = $this->_domToArray($dom);
-        }
-
+        $data = ComPagesDomDocument::fromString($string, true);
         return $object ? $this->merge($data) : $data;
     }
 
     public function toString()
     {
-        return $this->toDom()->saveHTML();
+        return ComPagesDomDocument::fromArray($this->toArray(), true)->toString();
     }
 
-    public function toDom()
+    public function fromArray($array, $object = true)
     {
-        $data   = $this->toArray();
-
-        $dom = new DOMDocumentHtml('1.0', 'UTF-8');
-        $dom->preserveWhiteSpace = false;
-        $dom->formatOutput = true;
-
-        $this->_arrayToDom($dom, $dom, $data);
-
-        return $dom;
-    }
-}
-
-class DomDocumentHtml extends DOMDocument
-{
-    private $__xpath;
-
-    public function __toString() {
-        return parent::saveHTML();
-    }
-
-    public function query($expression)
-    {
-        if(!isset($this->__xpath)) {
-            $this->__xpath =  new DOMXPath($this);
+        if($array instanceof KObjectConfigInterface) {
+            $array = $array->toArray();
         }
 
-        return $this->__xpath->query($expression);
-    }
-
-    public function evaluate($expression)
-    {
-        if(!isset($this->__xpath)) {
-            $this->__xpath =  new DOMXPath($this);
-        }
-
-        return $this->__xpath->evaluate($expression);
+        $document = ComPagesDomDocument::fromArray($array, true);
+        return $object ? $this->merge($document) : $document;
     }
 }
