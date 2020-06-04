b62d3425885e4b1dc7b447824da14e165fbc71d3
Johan Janssens <johan@janssens.me>
2020-06-04 03:40:13 +0200

    #361 - Recursively serialize the data

--- code/site/components/com_pages/view/json.php
+++ code/site/components/com_pages/view/json.php
@@ -231,23 +231,27 @@ class ComPagesViewJson extends KViewAbstract
     {
         $attributes = $entity->toArray();
 
-        //Cast objects to string
-        foreach($attributes as $key => $value)
+        //Recursively serialize the data
+        array_walk_recursive($attributes, function(&$value)
         {
-            //Qualify the url's
-            if($value instanceof KHttpUrlInterface) {
-                $value = $this->getUrl($value);
-            }
-
-            if(is_object($value))
+            if(!$value instanceof KModelEntityInterface)
             {
-                if(!method_exists($value, '__toString')) {
-                    unset($attributes[$key]);
-                } else {
-                    $attributes[$key] = (string) $value;
+                //Qualify the url's
+                if($value instanceof KHttpUrlInterface) {
+                    $value = $this->getUrl($value);
+                }
+
+                if(is_object($value))
+                {
+                    if(!method_exists($value, '__toString')) {
+                        $value = null;
+                    } else {
+                        $value = (string) $value;
+                    }
                 }
             }
-        }
+            else $value = $this->_getEntityAttributes($value);
+        });
 
         //Remove the identity key from the attributes
         $key = $entity->getIdentityKey();
