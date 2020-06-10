cb82d6bba80ad485bebaf094dcdcbb446de345ff
Johan Janssens <johan@janssens.me>
2020-06-10 07:52:12 +0200

    #368 - Add document.html template

---
+++ code/site/components/com_pages/resources/templates/document.html.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html xmlns:og="http://opengraphprotocol.org/schema/" lang="<?= language() ?>" dir="<?= direction() ?>" vocab="http://schema.org/">
+<head>
+    <meta charset="utf-8"/>
+    <base href="<?= url(); ?>" />
+
+    <ktml:title>
+    <ktml:meta>
+    <ktml:link>
+    <ktml:style>
+    <ktml:script>
+
+    <title><?= title() ?></title>
+
+</head>
+
+<body>
+
+<ktml:content>
+
+</body>
+</html>
