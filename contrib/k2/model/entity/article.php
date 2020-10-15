f8f7e67a37189538f957480415878b45ebc49e57
Johan Janssens <johan@janssens.me>
2020-10-15 10:41:36 +0200

    Fix typo

--- contrib/k2/model/entity/article.php
+++ contrib/k2/model/entity/article.php
@@ -7,7 +7,7 @@
  * @link        https://github.com/joomlatools/joomlatools-pages for the canonical source repository
  */
 
-class ExtK2ModelEntityArticle extends ExtJoomlaModelEntityAbstract
+class ExtK2ModelEntityArticle extends ExtK2ModelEntityAbstract
 {
 	protected function _initialize(KObjectConfig $config)
 	{
