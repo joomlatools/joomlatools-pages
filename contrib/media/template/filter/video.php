e0283007ebe9ab4c180175601cf9a684cb56d37e
Johan Janssens <johan@janssens.me>
2020-10-17 00:47:30 +0200

    # - Add client hints

--- contrib/media/template/filter/video.php
+++ contrib/media/template/filter/video.php
@@ -1,11 +1,18 @@
 <?php
+/**
+ * Joomlatools Pages
+ *
+ * @copyright   Copyright (C) 2018 Johan Janssens and Timble CVBA. (http://www.timble.net)
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        https://github.com/joomlatools/joomlatools-pages for the canonical source repository
+ */
 
 class ExtMediaTemplateFilterVideo extends ComPagesTemplateFilterAbstract
 {
     protected function _initialize(KObjectConfig $config)
     {
         $config->append(array(
-            'priority' => self::PRIORITY_LOWEST,
+            'priority' => self::PRIORITY_LOW,
             'enable'   => JDEBUG ? false : true,
         ));
 
@@ -20,8 +27,8 @@ class ExtMediaTemplateFilterVideo extends ComPagesTemplateFilterAbstract
     //See: https://github.com/aFarkas/lazysizes/blob/gh-pages/plugins/unveilhooks/ls.unveilhooks.js#L9
     public function filter(&$text)
     {
-        //Do not filter the images if we are rendering the page
-        if($this->getTemplate()->getLayout() !== NULL && $this->enabled())
+        //Filter the images only at the end of the rendering cycle
+        if($this->getTemplate()->getLayout() === false && $this->enabled())
         {
             $matches = array();
             if(preg_match_all('#<video\s([^>]*?[\'\"][^>]*?)>#iU', $text, $matches))
@@ -62,12 +69,11 @@ class ExtMediaTemplateFilterVideo extends ComPagesTemplateFilterAbstract
                         //Add lazyload
                         $attribs['class'][] = 'lazyload';
 
-
                         //Filter the images
                         $text = str_replace($matches[1][$key], $this->buildAttributes($attribs), $text);
 
                         //Import lazysizes
-                        $text .= $this->getTemplate()->helper('ext:media.video.import', 'unveilhooks');
+                        $text .= $this->getTemplate()->helper('video.import', 'unveilhooks');
                     }
                 }
             }
