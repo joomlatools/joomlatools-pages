0cbe4a5a57e22d4dd73a88a732ed40dbc20a233b
Johan Janssens <johan@janssens.me>
2020-10-17 00:56:29 +0200

    #436 - Add lazysizes helper

---
+++ contrib/media/template/helper/lazysizes.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Joomlatools Pages
+ *
+ * @copyright   Copyright (C) 2018 Johan Janssens and Timble CVBA. (http://www.timble.net)
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        https://github.com/joomlatools/joomlatools-pages for the canonical source repository
+ */
+
+class ExtMediaTemplateHelperLazysizes extends ComPagesTemplateHelperBehavior
+{
+    public function import($plugin = '', $config = array())
+    {
+        $config = new KObjectConfigJson($config);
+        $config->append(array(
+            'debug' =>  JFactory::getApplication()->getCfg('debug'),
+        ));
+
+        $html   = '';
+        $script = $plugin ? 'lazysizes-'.$plugin : 'lazysizes';
+        if (!static::isLoaded($script))
+        {
+            if($script == 'lazysizes')
+            {
+                $html .= '<ktml:script src="https://unpkg.com/lazysizes@5.2.2/lazysizes.'.(!$config->debug ? 'min.js' : 'js').'" defer="defer" />';
+                $html .= <<<LAZYSIZES
+<script>
+window.lazySizesConfig = window.lazySizesConfig || {};
+if ('connection' in navigator)
+{
+    //Only load nearby elements
+    if(navigator.connection.effectiveType.includes('2g')) {
+        window.lazySizesConfig.loadMode = 2
+    }
+
+    //Only load visible elements
+    if(navigator.connection.saveData === true) {
+        window.lazySizesConfig.loadMode = 1
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => 
+{
+     document.querySelectorAll('img[data-srclow]').forEach((img) => {
+         img.src = img.getAttribute('data-srclow');
+     });
+});
+</script>
+
+<style>
+img.progressive {
+    filter: blur(8px);
+    transform: scale(1.05);
+    transition: filter 400ms;
+}
+
+img.progressive.lazyloaded {
+    filter: blur(0);
+}
+</style>    
+LAZYSIZES;
+            }
+
+            if($script == 'lazysizes-bgset')
+            {
+                $html .= $this->import();
+                $html .= '<ktml:script src="https://unpkg.com/lazysizes@5.2.2/plugins/bgset/ls.bgset.' . (!$config->debug ? 'min.js' : 'js') . '" defer="defer" />';
+            }
+
+            if($script == 'lazysizes-unveilhooks')
+            {
+                $html .= $this->import();
+                $html .= '<ktml:script src="https://unpkg.com/lazysizes@5.2.2/plugins/unveilhooks/ls.unveilhooks.' . (!$config->debug ? 'min.js' : 'js') . '" defer="defer" />';
+            }
+
+            static::setLoaded($script);
+        }
+
+        return $html;
+    }
+}
