5f35554a04f808ec020890c121c9f2e80ad7ed79
Johan Janssens <johan@janssens.me>
2020-11-04 14:10:42 +0100

    Code formatting

--- code/site/components/com_pages/resources/mason/revalidator.js
+++ code/site/components/com_pages/resources/mason/revalidator.js
@@ -1,40 +1,35 @@
-const mason  = require('@joomlatools/mason-tools-v1');
-const https  = require('https');
-const http   = require('http');
-const chalk  = require('chalk');
+const mason = require('@joomlatools/mason-tools-v1');
+const https = require('https');
+const http = require('http');
+const chalk = require('chalk');
 
-class masonRevalidator
-{
+class masonRevalidator {
     options = {
-        origin      : null,
-        endpoint    : '/cache.json',
-        concurrency : 10,
-        force       : false,
+        origin: null,
+        endpoint: '/cache.json',
+        concurrency: 10,
+        force: false,
         unauthorized: false,
     }
 
-    constructor(options = {})
-    {
-        this.options = {...this.options, ...options }
+    constructor(options = {}) {
+        this.options = {...this.options, ...options}
 
         //Turn off certificate validation for TLS connections (only needed for self-signed certs)
-        if(this.options.unauthorized) {
+        if (this.options.unauthorized) {
             process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
         }
 
     }
 
-    async revalidate(url, collections)
-    {
+    async revalidate(url, collections) {
         const time = Date.now()
 
         //Cache origin url
-        if(this.options.origin)
-        {
+        if (this.options.origin) {
             const origin = new URL(this.options.origin)
 
-            if(url)
-            {
+            if (url) {
                 // Sanitize the url
                 if (url.indexOf('/') == 0) {
                     url = origin.origin + url
@@ -43,8 +38,7 @@ class masonRevalidator
                 if (!/^https?:\/\//i.test(url)) {
                     url = origin.protocol + '//' + url
                 }
-            }
-            else url = this.options.origin
+            } else url = this.options.origin
 
         }
 
@@ -52,11 +46,10 @@ class masonRevalidator
         const cache = new URL(this.options.endpoint, url)
 
         //Filter:id
-        if (url)
-        {
+        if (url) {
             let page = new URL(url)
 
-            if(url.substr(-1) == '/' || page.pathname.length > 1) {
+            if (url.substr(-1) == '/' || page.pathname.length > 1) {
                 cache.searchParams.set('id', page.pathname + page.search)
             }
         }
@@ -67,27 +60,26 @@ class masonRevalidator
         }
 
         //Filter:collection
-        if (collections)
-        {
-            if(collections instanceof Array) {
+        if (collections) {
+            if (collections instanceof Array) {
                 collections = collections.join(',')
             }
 
-            cache.searchParams.set('filter[collections]', 'in:'+collections)
+            cache.searchParams.set('filter[collections]', 'in:' + collections)
         }
 
         //Request options
-        var options = { }
-        if(cache.username && cache.password)
-        {
+        var options = {}
+        if (cache.username && cache.password) {
             let authorization = new Buffer.from(cache.username + ':' + cache.password).toString('base64')
-            options = { headers: {
-                    'Authorization' : 'Basic ' + authorization
-                }}
+            options = {
+                headers: {
+                    'Authorization': 'Basic ' + authorization
+                }
+            }
         }
 
-        this.getRequest(cache, options).then((response) =>
-        {
+        this.getRequest(cache, options).then((response) => {
             var data = response.json()
 
             //Check data
@@ -106,39 +98,33 @@ class masonRevalidator
             }
 
             //Get request time
-            mason.log.debug('\n',`${chalk.green('✔')} Cache fetched from: ${decodeURIComponent(cache.toString())} - ${response.time()} s` )
+            mason.log.debug('\n', `${chalk.green('✔')} Cache fetched from: ${decodeURIComponent(cache.toString())} - ${response.time()} s`)
 
             //Calculate the total elapsed time
-            let totalTime = () =>
-            {
+            let totalTime = () => {
                 let date = new Date(Date.now() - time)
                 let min = date.getUTCMinutes()
                 let sec = date.getUTCSeconds()
-                let ms  = ((date.getUTCMilliseconds() / 1000).toFixed(2)) * 1000;
+                let ms = ((date.getUTCMilliseconds() / 1000).toFixed(2)) * 1000;
 
-                return min + ":" + sec  + '.' +  ms
+                return min + ":" + sec + '.' + ms
             }
 
-            if (data.length != undefined)
-            {
-                if (data.length > 0)
-                {
+            if (data.length != undefined) {
+                if (data.length > 0) {
                     mason.log.debug(` ${chalk.green('✔')} Revalidation started  total: ${data.length}, concurrency: ${this.options.concurrency}…`)
 
                     // Async function to revalidate the cache with a concurrency limit
                     this.revalidateList(data, options).finally(() => {
-                        mason.log.debug('\n',chalk.bold(`Revalidation completed in ${totalTime()} min`))
+                        mason.log.debug('\n', chalk.bold(`Revalidation completed in ${totalTime()} min`))
                     })
-                }
-                else mason.log.debug('\n', chalk.bold(`Cache is valid`))
+                } else mason.log.debug('\n', chalk.bold(`Cache is valid`))
 
-            }
-            else
-            {
+            } else {
                 mason.log.debug(` ${chalk.green('✔')} Revalidation started, total: 1`)
 
                 this.revalidateUrl(data['attributes'], options).finally(() => {
-                    mason.log.debug('\n',chalk.bold(`Revalidation completed in ${totalTime()} min`))
+                    mason.log.debug('\n', chalk.bold(`Revalidation completed in ${totalTime()} min`))
                 })
             }
 
@@ -147,10 +133,8 @@ class masonRevalidator
         })
     }
 
-    async revalidateList(list, options)
-    {
-        for (let i = 0; i < list.length; i += this.options.concurrency)
-        {
+    async revalidateList(list, options) {
+        for (let i = 0; i < list.length; i += this.options.concurrency) {
             const requests = list.slice(i, i + this.options.concurrency).map((item) => {
                 return this.revalidateUrl(item['attributes'], options)
             })
@@ -159,8 +143,7 @@ class masonRevalidator
         }
     }
 
-    async revalidateUrl(item, options)
-    {
+    async revalidateUrl(item, options) {
         let url = item['url']
         let defaults = {
             headers: {
@@ -171,47 +154,40 @@ class masonRevalidator
 
         options = {...defaults, ...options}
 
-        let request = this.getRequest(url, options).then((response) =>
-        {
-            let hash   = response.hash()
-            let time   = response.time()
+        let request = this.getRequest(url, options).then((response) => {
+            let hash = response.hash()
+            let time = response.time()
             let status = response.headers['cache-status'].toLowerCase()
 
-            if(hash == null || hash != item['hash'])
-            {
-                if(status.includes('purged')) {
+            if (hash == null || hash != item['hash']) {
+                if (status.includes('purged')) {
                     mason.log.debug(chalk.red('  » Purged: ' + url + ' - ' + time + ' s'))
                 } else {
                     mason.log.debug('  » Regenerated: ' + url + ' - ' + time + ' s')
                 }
-            }
-            else mason.log.debug(chalk.gray('  » Not Modified: ' + url + ' - ' + time + ' s'))
+            } else mason.log.debug(chalk.gray('  » Not Modified: ' + url + ' - ' + time + ' s'))
 
-        }).catch((error)  => {
+        }).catch((error) => {
             mason.log.error('Failed: ' + url + ' with error: ' + error.message)
         })
 
         return request
     }
 
-    async getRequest(url, options)
-    {
+    async getRequest(url, options) {
         options = options || {}
 
-        let promise = new Promise((resolve, reject) =>
-        {
-            let callback = (response) =>
-            {
+        let promise = new Promise((resolve, reject) => {
+            let callback = (response) => {
                 let data = ''
 
                 response.on('data', (chunk) => {
                     data += chunk
                 })
 
-                response.on('end', () =>
-                {
+                response.on('end', () => {
                     response.data = data
-                    resolve (response)
+                    resolve(response)
                 })
 
                 response.on('error', (error) => {
@@ -222,57 +198,48 @@ class masonRevalidator
                     return JSON.parse(data)
                 }
 
-                response.hash = () =>
-                {
+                response.hash = () => {
                     let hash = null
 
-                    if(response.headers.etag) {
+                    if (response.headers.etag) {
                         hash = response.headers.etag.replace('W/', '').replace('-gzip', '')
                     }
 
                     return hash
                 }
 
-                response.time = (metric = 'tot') =>
-                {
-                    let time   = null
+                response.time = (metric = 'tot') => {
+                    let time = null
                     let timing = response.timing();
 
-                    if(timing[metric])
-                    {
+                    if (timing[metric]) {
                         metric = timing[metric];
-                        time   = (metric.dur / 1000).toFixed(2);
+                        time = (metric.dur / 1000).toFixed(2);
                     }
 
                     return time;
                 }
 
-                response.timing = () =>
-                {
+                response.timing = () => {
                     var timing = {};
 
-                    if(response.headers['server-timing'])
-                    {
+                    if (response.headers['server-timing']) {
                         let timings = response.headers['server-timing'].split(',')
 
-                        timings.forEach((metric) =>
-                        {
+                        timings.forEach((metric) => {
                             metric = metric.split(';');
 
                             var values = {};
-                            var name   = '';
+                            var name = '';
 
-                            metric.forEach((part) =>
-                            {
-                                if(part.includes('='))
-                                {
-                                    var [key, value]  = part.split('=');
-                                    values[key] = value.replace(/"/g,'')
-                                }
-                                else name = part;
+                            metric.forEach((part) => {
+                                if (part.includes('=')) {
+                                    var [key, value] = part.split('=');
+                                    values[key] = value.replace(/"/g, '')
+                                } else name = part;
                             })
 
-                            if(name) {
+                            if (name) {
                                 timing[name] = values;
                             }
                         })
@@ -287,7 +254,7 @@ class masonRevalidator
             //Create the request based on the protocol
             let protocol = url.protocol.replace(':', '');
 
-            if(protocol == 'https') {
+            if (protocol == 'https') {
                 var request = https.get(url, options, callback);
             } else {
                 var request = http.get(url, options, callback);
@@ -304,14 +271,13 @@ class masonRevalidator
     }
 }
 
-async function revalidateCache(config = {})
-{
+async function revalidateCache(config = {}) {
     const argv = require('yargs').argv
 
     config = mason.config.merge({
-        force       : argv.force,
+        force: argv.force,
         unauthorized: argv.u,
-        concurrency : argv.concurrency != null ? argv.concurrency : 10,
+        concurrency: argv.concurrency != null ? argv.concurrency : 10,
     }, config)
 
     //Revaliate cache
