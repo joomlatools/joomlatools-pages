e56ec8bed5efd3fcd1da571d84ef74a728008a30
Johan Janssens <johan@janssens.me>
2021-03-12 21:01:14 +0100

    #586 - Automatic link prefetching
    Attach the link prefetcher behavior automatically to the page if:
    
    - The page has format html and is cacheable
    - Static cache has been enabled
    
    Change default selector to a.prefetch and allow to enable/disable and
    override the prefetch selector using the a new process/prefetch
    page and layout attribute

---
+++ code/site/components/com_pages/event/subscriber/prefetcher.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Joomlatools Pages
+ *
+ * @copyright   Copyright (C) 2018 Johan Janssens and Timble CVBA. (http://www.timble.net)
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        https://github.com/joomlatools/joomlatools-pages for the canonical source repository
+ */
+
+class ComPagesEventSubscriberPrefetcher extends ComPagesEventSubscriberAbstract
+{
+    protected function _initialize(KObjectConfig $config)
+    {
+        $config->append(array(
+            'options'  => [
+                'selector' => 'a.prefetch',
+                'onload'   => true,
+                'onhover'  => true,
+                'debug'    => JDEBUG
+            ],
+        ));
+
+        parent::_initialize($config);
+    }
+
+    public function onBeforeDispatcherSend(KEventInterface $event)
+    {
+        $content = $event->getTarget()->getResponse()->getContent();
+
+        if($event->getTarget()->getRequest()->getFormat() == 'html' && $event->getTarget()->isCacheable(true))
+        {
+            $page     = $event->getTarget()->getPage();
+            $prefetch = $page->get('process/prefetch',  $this->getObject('com://site/pages.config')->get('http_static_cache', false));
+
+            if($prefetch !== false)
+            {
+                $template = $event->getTarget()->getController()->getView()->getTemplate();
+
+                $config = $this->getConfig()->options;
+                $config->selector = is_string($prefetch) ? $prefetch : 'a.prefetch';
+
+                $prefetcher = $template->helper('behavior.prefetcher', $config);
+                $template->getFilter('asset')->filter($prefetcher);
+
+                $event->getTarget()->getResponse()->setContent($content.$prefetcher);
+            }
+        }
+    }
+}
