418f8cd656b44216d24ab278e36487769cbab619
Johan Janssens <johan@janssens.me>
2021-04-17 01:06:32 +0200

    #653 - Move pathway logic

--- code/site/components/com_pages/event/subscriber/dispatcher.php
+++ code/site/components/com_pages/event/subscriber/dispatcher.php
@@ -191,6 +191,49 @@ class ComPagesEventSubscriberDispatcher extends ComPagesEventSubscriberAbstract
         }
     }
 
+    public function onAfterApplicationDispatch(KEventInterface $event)
+    {
+        if($this->isDispatched())
+        {
+            //Set the path in the pathway to allow for module injection
+            $page_route = $this->getDispatcher()->getRoute()->getPath(false);
+
+            if($menu = JFactory::getApplication()->getMenu()->getActive()) {
+                $menu_route = $menu->route;
+            } else {
+                $menu_route = '';
+            }
+
+            if($path = ltrim(str_replace($menu_route, '', $page_route), '/'))
+            {
+                $pathway = JFactory::getApplication()->getPathway();
+                $router = $this->getObject('router');
+
+                $segments = array();
+                foreach(explode('/', $path) as $segment)
+                {
+                    $segments[] = $segment;
+
+                    if($route = $router->generate('pages:'.implode('/', $segments)))
+                    {
+                        $page = $route->getPage();
+
+                        if(!$page->name) {
+                            $name = ucwords(str_replace(array('_', '-'), ' ', $page->slug));
+                        } else {
+                            $name = ucfirst($page->name);
+                        }
+
+                        $route = $router->qualify($route);
+                        $url   = $route->toString(KHttpUrl::PATH);
+
+                        $pathway->addItem($name, (string) $url);
+                    }
+                }
+            }
+        }
+    }
+
     public function onAfterApplicationRender(KEventInterface $event)
     {
         if(!headers_sent())
