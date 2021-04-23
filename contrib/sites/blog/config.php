6b0bd064df297fd368c8bb3d35b9d3df8fbee821
Johan Janssens <johan@janssens.me>
2021-04-23 23:26:49 +0200

    #561 - Cleanup config.php

--- contrib/sites/blog/config.php
+++ contrib/sites/blog/config.php
@@ -1,17 +1,10 @@
 <?php
 
-//Load server environment
-if(isset($_SERVER['HOME']) && file_exists($_SERVER['HOME'].'/private_html/.env.'.getenv('SITE')))
-{
-  $dotenv = new Symfony\Component\Dotenv\Dotenv();
-  $dotenv->usePutenv()->load($_SERVER['HOME'].'/private_html/.env.'.getenv('SITE'));
-}
-
 return array(
 
     // Site
     'site' => [
-        'name'              => 'Joomlatools Pages & Joomla',
+        'name'  => 'Joomlatools Pages - Blog example',
     ],
 
     // Page
@@ -26,19 +19,19 @@ return array(
             'og:image'      => '',
             'twitter:site'  => '',
             'twitter:card'  => '',
-            'fb:admins'     => '',
         ],
 
         'visible'   => true,
         'published' => true,
     ],
 
+    // Aliases
     'aliases' => [
-        'theme://'                  => getenv('SITE')  ? '/theme/' : '/joomlatools-pages/theme/',
-        'images://'                 => getenv('SITE') ? '/images/'    : '/joomlatools-pages/images/',
+        'theme://'   => '/joomlatools-pages/theme/',
+        'images://'  => '/joomlatools-pages/images/',
     ],
 
     // Google Analytics
-    'ga_code' => getenv('SITE') ? '' : '',
+    'ga_code' => '',
 
 );
