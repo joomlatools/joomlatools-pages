9cb7f5caa72da26cb3164a2335177022d35f3dd4
Johan Janssens <johan@janssens.me>
2021-04-27 01:47:15 +0200

    #175 - Add ext:joomla.model.menus

--- contrib/extensions/joomla/config.php
+++ contrib/extensions/joomla/config.php
@@ -1,8 +1,9 @@
 <?php
 return [
-
     'page.registry' => [
-        'collections' => ['articles' => ['model' => 'ext:joomla.model.articles']]
+        'collections' => [
+            'articles' => ['model' => 'ext:joomla.model.articles'],
+            'menus'    => ['model' => 'ext:joomla.model.menus']
+        ]
     ]
-
 ];
