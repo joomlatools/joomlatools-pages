2840c527eafc5dea49059b4cd0d9a893ec3fd64a
Johan Janssens <johan@janssens.me>
2021-04-28 23:53:31 +0200

    #679 - Rename ComPagesDispatcherRouterPages to ComPagesDispatcherRouter

--- code/site/components/com_pages/dispatcher/router/router.php
+++ code/site/components/com_pages/dispatcher/router/router.php
@@ -7,128 +7,30 @@
  * @link        https://github.com/joomlatools/joomlatools-pages for the canonical source repository
  */
 
-/**
- * Dispatcher Router Singleton
- *
- * Force the router object to a singleton with identifier alias 'router'.
- *
- * @author  Johan Janssens <https://github.com/johanjanssens>
- * @package Koowa\Library\Dispatcher\Router
- */
-class ComPagesDispatcherRouter extends ComPagesDispatcherRouterAbstract implements KObjectSingleton
+class ComPagesDispatcherRouter extends ComPagesDispatcherRouterAbstract
 {
-    /**
-     * The routers
-     *
-     * @var	array
-     */
-    private $__routers;
-
-    public function __construct(KObjectConfig $config)
+    protected function _initialize(KObjectConfig $config)
     {
-        parent::__construct($config);
+        $config->append([
+            'route'  => 'page',
+            'routes' => $this->getObject('page.registry')->getRoutes(),
+        ])->append(([
+            'resolvers' => [
+                'state',
+                'pagination',
+                'regex' => ['routes' => $config->routes],
+            ]
+        ]));
 
-        //Add a global object alias
-        $this->getObject('manager')->registerAlias($this->getIdentifier(), 'router');
+        parent::_initialize($config);
     }
 
-    /**
-     * Resolve a route
-     *
-     * Proxy to the specific component router
-     *
-     * @param string|ComPagesDispatcherRouterRouteInterface|KObjectInterface $route The route to resolve
-     * @param array $parameters Route parameters
-     * @return false| ComPagesDispatcherRouterInterface Returns the matched route or false if no match was found
-     */
-    public function resolve($route, array $parameters = array())
+    public function getRoute($route, array $parameters = array())
     {
-        $result = false;
-
-        //Find router
-        if($route instanceof KObjectInterface)
-        {
-            $identifier			= $route->getIdentifier()->toArray();
-            $identifier['path']	= array('dispatcher', 'router');
-
-            if(!$route instanceof ComPagesDispatcherRouterRouteInterface) {
-                $identifier['name']	= $route->getIdentifier()->getPackage();
-            } else {
-                $identifier['name']	=  $route->getScheme();
-            }
-
-            $identifier = (string) $this->getIdentifier($identifier);
-        }
-        else
-        {
-            $package  = parse_url($route, PHP_URL_SCHEME);
-            $identifier = 'com://site/'.$package.'.dispatcher.router.'.$package;
+        if($route instanceof ComPagesModelEntityPage) {
+            $route = 'pages:'.$route->path;
         }
 
-        //Get router instance
-        if(!isset($this->__routers[$identifier]))
-        {
-            $config = [
-                'request'   => $this->getRequest(),
-                'resolvers' => $this->getResolvers()
-            ];
-
-            $router =  $this->getObject($identifier, $config);
-
-            $this->__routers[$identifier] = $router;
-        }
-        else $router = $this->__routers[$identifier];
-
-        return $router->resolve($route, $parameters);
-    }
-
-    /**
-     * Generate a route
-     *
-     *  Proxy to the specific component router
-     *
-     * @param string|ComPagesDispatcherRouterRouteInterface|KObjectInterface $route The route to resolve
-     * @param array $parameters Route parameters
-     * @return false|KHttpUrlInterface Returns the generated route
-     */
-    public function generate($route, array $parameters = array())
-    {
-        $result = false;
-
-        //Find router
-        if($route instanceof KObjectInterface)
-        {
-            $identifier			= $route->getIdentifier()->toArray();
-            $identifier['path']	= array('dispatcher', 'router');
-
-            if(!$route instanceof ComPagesDispatcherRouterRouteInterface) {
-                $identifier['name']	= $route->getIdentifier()->getPackage();
-            } else {
-                $identifier['name']	=  $route->getScheme();
-            }
-
-            $identifier = (string) $this->getIdentifier($identifier);
-        }
-        else
-        {
-            $package  = parse_url($route, PHP_URL_SCHEME);
-            $identifier = 'com://site/'.$package.'.dispatcher.router.'.$package;
-        }
-
-        //Get router instance
-        if(!isset($this->__routers[$identifier]))
-        {
-            $config = [
-                'request'   => $this->getRequest(),
-                'resolvers' => $this->getResolvers()
-            ];
-
-            $router =  $this->getObject($identifier, $config);
-
-            $this->__routers[$identifier] = $router;
-        }
-        else $router = $this->__routers[$identifier];
-
-        return $router->generate($route, $parameters);
+        return parent::getRoute($route, $parameters);
     }
 }
