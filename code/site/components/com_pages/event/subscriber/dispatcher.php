5ed68e347acfe5a9ef2152addbf9397d2ef831b6
Johan Janssens <johan@janssens.me>
2021-05-11 19:30:41 +0200

    #175 - Move Joomla template, pathway csrf token handling to Joomla extension

--- code/site/components/com_pages/event/subscriber/dispatcher.php
+++ code/site/components/com_pages/event/subscriber/dispatcher.php
@@ -39,11 +39,6 @@ class ComPagesEventSubscriberDispatcher extends ComPagesEventSubscriberAbstract
         parent::_initialize($config);
     }
 
-    public function onAfterKoowaBootstrap()
-    {
-        $this->attachEventHandler('onAfterModuleList', 'filterTemplateModules');
-    }
-
     public function onAfterApplicationInitialise(KEventInterface $event)
     {
         $dispatcher = $this->getDispatcher();
@@ -98,17 +93,6 @@ class ComPagesEventSubscriberDispatcher extends ComPagesEventSubscriberAbstract
                     }
 
                 },  JRouter::PROCESS_BEFORE);
-            }
-        }
-
-        //Authenticate anonymous requests and inject form token dynamically
-        if($dispatcher->getRequest()->isPost() && $dispatcher->isCacheable())
-        {
-            if($cache = $dispatcher->loadCache())
-            {
-                if($application->input->request->get($cache['token'])) {
-                    $application->input->post->set(JSession::getFormToken(), '1');
-                }
             }
         }
     }
@@ -187,156 +171,6 @@ class ComPagesEventSubscriberDispatcher extends ComPagesEventSubscriberAbstract
 
                 //Manually set the cache status
                 JFactory::getApplication()->setHeader('Cache-Status', 'DYNAMIC, PURGED');
-            }
-        }
-    }
-
-    public function onAfterApplicationDispatch(KEventInterface $event)
-    {
-        if($this->isDispatchable())
-        {
-            //Set the path in the pathway to allow for module injection
-            $page_route = $this->getDispatcher()->getRoute()->getPath(false);
-
-            if($menu = JFactory::getApplication()->getMenu()->getActive()) {
-                $menu_route = $menu->route;
-            } else {
-                $menu_route = '';
-            }
-
-            if($path = ltrim(str_replace($menu_route, '', $page_route), '/'))
-            {
-                $pathway = JFactory::getApplication()->getPathway();
-                $router = $this->getObject('router');
-
-                $segments = array();
-                foreach(explode('/', $path) as $segment)
-                {
-                    $segments[] = $segment;
-
-                    if($route = $router->generate('pages:'.implode('/', $segments)))
-                    {
-                        $page = $route->getPage();
-
-                        if(!$page->name) {
-                            $name = ucwords(str_replace(array('_', '-'), ' ', $page->slug));
-                        } else {
-                            $name = ucfirst($page->name);
-                        }
-
-                        $route = $router->qualify($route);
-                        $url   = $route->toString(KHttpUrl::PATH);
-
-                        $pathway->addItem($name, (string) $url);
-                    }
-                }
-            }
-        }
-    }
-
-    public function onAfterApplicationRender(KEventInterface $event)
-    {
-        if(!headers_sent())
-        {
-            header_register_callback( function() {
-                header_remove('Expires');
-            });
-        }
-    }
-
-    public function onAfterApplicationRespond(KEventInterface $event)
-    {
-        //Cache and cleanup Joomla output if routing to a page
-        if($this->isDispatchable())
-        {
-            $headers = array();
-            foreach (headers_list() as $header)
-            {
-                $parts = explode(':', $header, 2);
-                $headers[trim($parts[0])] = trim($parts[1]);
-            }
-
-            //Remove the Expires header
-            unset($headers['Expires']);
-
-            //Do not cache if Joomla is running in debug mode
-            $dispatcher = $this->getDispatcher();
-            if(!JDEBUG && $dispatcher->isCacheable() && $dispatcher->isDecorated())
-            {
-                $content = $event->getTarget()->getBody();
-
-                $dispatcher->getResponse()->setHeaders($headers);
-                $dispatcher->getResponse()->setContent($content);
-
-                $dispatcher->cache();
-            }
-        }
-    }
-
-    public function onBeforeDispatcherCache(KEventInterface $event)
-    {
-        if($this->isDispatchable())
-        {
-            $dispatcher = $this->getDispatcher();
-            $content    = $dispatcher->getResponse()->getContent();
-
-            //Replace the session based form and csrf token with a fixed token
-            $token = $dispatcher->getCacheToken();
-
-            $search      = '#<input type="hidden" name="[0-9a-f]{32}" value="1" />#';
-            $replacement = '<input type="hidden" name="' . $token . '" value="1" />';
-
-            $content = preg_replace($search, $replacement, $content);
-
-            //Search for a csrf token in the content and refresh it
-            $search      = '#"csrf.token":\s*"[0-9a-f]{32}"#';
-            $replacement = '"csrf.token": "' . $token . '"';
-
-            $content = preg_replace($search, $replacement, $content);
-
-            $dispatcher->getResponse()->setContent($content);
-        }
-    }
-
-    public function filterTemplateModules(&$modules)
-    {
-        if($this->isDispatchable())
-        {
-            $page = $this->getDispatcher()->getPage();
-
-            if($page->has('process/template/modules'))
-            {
-                if(count($page->get('process/template/modules')))
-                {
-                    foreach ($page->get('process/template/modules') as $key => $module)
-                    {
-                        $include = array();
-                        $exclude = array();
-
-                        foreach ($page->process->template->get('modules') as $id)
-                        {
-                            if ($id[0] == '-' || $id < 0) {
-                                $exclude[] = substr($id, 1);
-                            } else {
-                                $include[] = $id;
-                            }
-                        }
-
-                        if ($include)
-                        {
-                            if (!in_array($module->title, $include) && !in_array($module->id, $include)) {
-                                unset($modules[$key]);
-                            }
-                        }
-                        else
-                        {
-                            if (in_array($module->title, $exclude) || in_array($module->id, $exclude)) {
-                                unset($modules[$key]);
-                            }
-                        }
-                    }
-                }
-                else $modules = array();
             }
         }
     }
