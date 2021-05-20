be16c56f69176e840a879939b8464594b98ad89d
Johan Janssens <johan@janssens.me>
2021-05-20 22:57:50 +0200

    #368 - Add new partial() template method to make including partials easier

---
+++ code/template/function/partial.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Joomlatools Pages
+ *
+ * @copyright   Copyright (C) 2018 Johan Janssens and Timble CVBA. (http://www.timble.net)
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        https://github.com/joomlatools/joomlatools-pages for the canonical source repository
+ */
+
+return function($url, $data = array())
+{
+    //Qualify the template
+    if(!parse_url($url, PHP_URL_SCHEME)) {
+        $url = 'template:partials/'.trim($url, '/');
+    }
+
+    return $this->import($url, $data);
+};
