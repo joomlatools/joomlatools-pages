c041686f535f3c74fd953ff47d7d80533200271a
Johan Janssens <johan@janssens.me>
2021-06-04 23:48:12 +0200

    #731 - Fix path

--- contrib/sites/blog/templates/partials/navigation/footer.html.php
+++ contrib/sites/blog/templates/partials/navigation/footer.html.php
@@ -1,5 +1,5 @@
 <? foreach (data('navigation/footer') as $item) : ?>
-<a <? if($item->route) : ?>href="<?= route($item->route); ?>"<? else: ?>rel="nofollow" href="<?= $item->url ?>"<? endif ?> class="text-black no-underline hover:underline ml-4">
+<a <? if($item->path) : ?>href="<?= route($item->path); ?>"<? else: ?>rel="nofollow" href="<?= $item->url ?>"<? endif ?> class="text-black no-underline hover:underline ml-4">
 	<?= $item->title; ?>
 </a>
 <? endforeach ?>
