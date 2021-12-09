410dfce72cbc07c52c4bbf282331ccca4adddb90
Johan Janssens <johan@janssens.me>
2021-12-09 12:43:58 +0100

    #845 - Add Sentry config init options

--- contrib/extensions/sentry/event/subscriber/exception.php
+++ contrib/extensions/sentry/event/subscriber/exception.php
@@ -9,7 +9,38 @@
 
 class ExtSentryEventSubscriberException extends ComPagesEventSubscriberAbstract
 {
-    public function onException(KEvent $event)
+    protected function _initialize(KObjectConfig $config)
+    {
+        $config->append(array(
+            'dsn'         => getenv('SENTRY_DSN'),
+            'environment' => getenv('SENTRY_ENVIONMENT'),
+            'traces_sample_rate' => 1.0,
+            'release'     => getenv('SENTRY_RELEASE') ?: $this->getObject('com:pages.version')->getVersion(),
+            'tags'        => array()
+        ));
+
+        parent::_initialize($config);
+    }
+
+    public function onAfterKoowaBootstrap(KEventInterface $event)
+    {
+        \Sentry\init([
+            'dsn'                => $this->getConfig()->dsn,
+            'environment'        => $this->getConfig()->environment ?: null,
+            'traces_sample_rate' => $this->getConfig()->traces_sample_rate,
+            'release'            => $this->getConfig()->release,
+        ]);
+
+        //Configure Sentry
+        \Sentry\configureScope(function (\Sentry\State\Scope $scope): void
+        {
+            foreach($this->getConfig()->tags as $key => $value) {
+                $scope->setTag($key, $value);
+            }
+        });
+    }
+
+    public function onException(KEventInterface $event)
     {
         $exception = $event->exception;
 
@@ -20,6 +51,6 @@ class ExtSentryEventSubscriberException extends ComPagesEventSubscriberAbstract
 
     public function isEnabled()
     {
-        return getenv('SENTRY_DSN') && function_exists('\Sentry\captureException') && parent::isEnabled();
+        return $this->getConfig()->dsn && function_exists('\Sentry\captureException') && parent::isEnabled();
     }
 }
