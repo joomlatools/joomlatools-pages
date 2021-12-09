5a19378a648be19a118a6929a5a69e35584a5f3b
Johan Janssens <johan@janssens.me>
2021-12-09 12:39:17 +0100

    #845 - Add `release` config option

--- contrib/extensions/sentry/resources/templates/script.html.php
+++ contrib/extensions/sentry/resources/templates/script.html.php
@@ -20,6 +20,7 @@
 
 dsn:
 version:
+release:
 tunnel:
 tags: []
 environment:
@@ -29,6 +30,7 @@ tracesSampleRate: 1.0
 <?
 $dsn = $dsn ?? getenv('SENTRY_DSN');
 $env = $environment ?? getenv('SENTRY_ENVIRONMENT');
+$rel = $release ?? getenv('SENTRY_RELEASE') ?: $this->getObject('com:pages.version')->getVersion();
 
 if(!empty($version)) {
     $version =  '@'.$version;
@@ -43,6 +45,7 @@ Sentry.init({
     dsn: "<?= $dsn ?>",
     debug: <?= debug() ? 'true' : 'false' ?>,
     tunnel: <?= !empty($tunnel) ? '"'.$tunnel.'"' : 'null' ?>,
+    release: <?= !empty($rel) ? '"'.$rel.'"' : 'null' ?>,
     environment: <?= !empty($env) ? '"'.$env.'"' : 'null' ?>,
     tracesSampleRate: <?= $tracesSampleRate ?? 1.0 ?>,
     integrations: [new Sentry.Integrations.BrowserTracing()],
