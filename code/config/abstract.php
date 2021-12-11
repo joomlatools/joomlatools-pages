9727f9369ba9e969bfa0b795a0c5984883c041c4
Johan Janssens <johan@janssens.me>
2021-12-11 02:31:06 +0100

    #846 - Implement __call() to handle get[Option] to get->option

--- code/config/abstract.php
+++ code/config/abstract.php
@@ -11,7 +11,7 @@ class ComPagesConfigAbstract extends KObject implements ComPagesConfigInterface
 {
     public function get($option, $default = null)
     {
-        $method = 'get'.str_replace(' ', '', ucwords(strtolower(str_replace('_', ' ', $option))));
+        $method = 'get'.KStringInflector::camelize($option);
         if(!method_exists($this, $method) ) {
             $value = $this->getConfig()->get($option, $default);
         } else {
@@ -29,5 +29,19 @@ class ComPagesConfigAbstract extends KObject implements ComPagesConfigInterface
     final public function __get($key)
     {
         return $this->get($key);
+    }
+
+    public function __call($method, $arguments)
+    {
+       if(strpos( $method , 'get' ) === 0)
+       {
+           if($option = substr($method, 3))
+           {
+               $option = KStringInflector::underscore($option);
+               return $this->get($option);
+           }
+       }
+
+       return parent::__call($method, $arguments);
     }
 }
