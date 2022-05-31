4c44f8a1fef26a000cfc44f2ffe30c796b5813fb
waseemsadiq <waseem2202@gmail.com>
2022-05-31 23:57:32 +0100

    #55 Add 'filter' => ['visible' => 'neq:false'] back in to mainmenu
    In order to show how to hide a page from the generated menu list

--- contrib/sites/blog/templates/partials/navigation/mainmenu.html.php
+++ contrib/sites/blog/templates/partials/navigation/mainmenu.html.php
@@ -1,4 +1,4 @@
-<? $pages = collection('pages', ['folder' => $folder ?? '.', 'level' => 1,  'recurse' => 'true'])  ?>
+<? $pages = collection('pages', ['folder' => $folder ?? '.', 'level' => 1,  'recurse' => 'true', 'filter' => ['visible' => 'neq:false']])  ?>
 <? foreach ($pages as $page) :?>
     <a class="px-2 md:pl-0 md:mr-3 md:pr-3 text-gray-700 no-underline md:border-r border-gray-light" href="<?= route($page); ?>"><?= $page->name; ?></a>
 <? endforeach ?>
