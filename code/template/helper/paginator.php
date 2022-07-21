42a07c03147f91134d35dd888b177d3c26049ebd
Johan Janssens <johan@janssens.me>
2022-07-22 00:14:45 +0200

    #943 - Need to merge config options with object

--- code/template/helper/paginator.php
+++ code/template/helper/paginator.php
@@ -14,6 +14,7 @@ class ComPagesTemplateHelperPaginator extends KTemplateHelperPaginator
     public function pagination($config = array())
     {
         $config = new KObjectConfigJson($config);
+        $config->append($this->getConfig());
         $config->append(array(
             'total'      => 0,
             'display'    => 2,
